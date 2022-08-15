38babbab7f372d1eccd844534c5c4c8bbc6ae5ea
sahanivinay <99119816+sahanivinay@users.noreply.github.com>
2022-08-15 22:03:57 +0530

    Update File_Extension.py

--- File_Extension.py
+++ File_Extension.py
@@ -1,3 +1,7 @@
-fileName = input("Enter the filename: ")
-file_extension = fileName.split(".")
-print("The extension of file is "+ repr(file_extension[-1]))
+import os
+file = input("Enter the file name: ")
+file_extn = os.path.splitext(file)
+file_name = file_extn[0]
+file_extension = file_extn[1]
+print("File Name is: ",file_name)
+print("File Extension is: ",file_extension)
